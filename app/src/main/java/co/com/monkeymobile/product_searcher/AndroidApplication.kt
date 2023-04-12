701014a7c1f308d40ea036fa43b1ba42239caadc
Hector A. Narvaez <hanarvaez92@gmail.com>
2023-04-12 16:43:35 -0500

    Added AndroidApplication class

---
+++ app/src/main/java/co/com/monkeymobile/product_searcher/AndroidApplication.kt
@@ -0,0 +1,7 @@
+package co.com.monkeymobile.product_searcher
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AndroidApplication : Application()
