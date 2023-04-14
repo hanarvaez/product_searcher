ac81fbd82fa3ec00cf0a6d060fc4f5cdba24fa05
Hector A. Narvaez <hanarvaez92@gmail.com>
2023-04-14 02:47:52 -0500

    Added use case for fetch items list

---
+++ app/src/main/java/co/com/monkeymobile/product_searcher/domain/use_case/GetItemListUseCase.kt
@@ -0,0 +1,25 @@
+package co.com.monkeymobile.product_searcher.domain.use_case
+
+import co.com.monkeymobile.product_searcher.di.DefaultDispatcher
+import co.com.monkeymobile.product_searcher.domain.model.Item
+import co.com.monkeymobile.product_searcher.domain.repository.ItemListRepository
+import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.CoroutineDispatcher
+
+@ViewModelScoped
+class GetItemListUseCase(
+    private val itemListRepository: ItemListRepository,
+    @DefaultDispatcher dispatcher: CoroutineDispatcher
+) : SuspendUseCase<GetItemListUseCaseParams, GetItemListUseCaseResult>(dispatcher) {
+
+    override suspend fun execute(parameters: GetItemListUseCaseParams) = GetItemListUseCaseResult(
+        itemListRepository.fetchItemList(
+            parameters.siteId,
+            parameters.query
+        )
+    )
+}
+
+data class GetItemListUseCaseParams(val siteId: String, val query: String)
+
+data class GetItemListUseCaseResult(val items: List<Item>)
