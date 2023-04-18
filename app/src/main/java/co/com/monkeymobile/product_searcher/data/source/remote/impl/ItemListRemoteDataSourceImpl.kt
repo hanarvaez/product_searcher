712a2bd4e1d51aa1e71280bc7c09d8978d8e2093
Hector A. Narvaez <hanarvaez92@gmail.com>
2023-04-18 14:47:34 -0500

    Bugfix: Added missing Inject constructors into ItemListRemoteDataSourceImpl.kt and GetItemListUseCase.kt

--- app/src/main/java/co/com/monkeymobile/product_searcher/data/source/remote/impl/ItemListRemoteDataSourceImpl.kt
+++ app/src/main/java/co/com/monkeymobile/product_searcher/data/source/remote/impl/ItemListRemoteDataSourceImpl.kt
@@ -2,10 +2,11 @@ package co.com.monkeymobile.product_searcher.data.source.remote.impl
 
 import co.com.monkeymobile.product_searcher.data.source.remote.ItemListRemoteDataSource
 import co.com.monkeymobile.product_searcher.di.NetworkModule
+import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class ItemListRemoteDataSourceImpl : ItemListRemoteDataSource {
+class ItemListRemoteDataSourceImpl @Inject constructor(): ItemListRemoteDataSource {
 
     override suspend fun fetchItemList(siteId: String, query: String) = NetworkModule
         .getApiService()
--- app/src/main/java/co/com/monkeymobile/product_searcher/domain/use_case/GetItemListUseCase.kt
+++ app/src/main/java/co/com/monkeymobile/product_searcher/domain/use_case/GetItemListUseCase.kt
@@ -5,9 +5,10 @@ import co.com.monkeymobile.product_searcher.domain.model.Item
 import co.com.monkeymobile.product_searcher.domain.repository.ItemListRepository
 import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Inject
 
 @ViewModelScoped
-class GetItemListUseCase(
+class GetItemListUseCase @Inject constructor(
     private val itemListRepository: ItemListRepository,
     @DefaultDispatcher dispatcher: CoroutineDispatcher
 ) : SuspendUseCase<GetItemListUseCaseParams, GetItemListUseCaseResult>(dispatcher) {
