4bba7d36f591149f205530041e6f211200a003f2
Hector A. Narvaez <hanarvaez92@gmail.com>
2023-04-19 17:46:56 -0500

    Added ProductDetailActivity

---
+++ app/src/main/java/co/com/monkeymobile/product_searcher/presentation/product_detail/ProductDetailActivity.kt
@@ -0,0 +1,12 @@
+package co.com.monkeymobile.product_searcher.presentation.product_detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.com.monkeymobile.product_searcher.R
+
+class ProductDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_detail)
+    }
+}
